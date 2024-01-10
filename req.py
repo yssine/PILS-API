85b9efeecded5f2d8d974eb060c2eca50d9ff4b7
YsN <ysn@YsN.lyon2.boamuo.quantic.ninja>
2024-01-10 11:20:08 +0100

    db reset

--- req.py
+++ req.py
@@ -1,20 +1,23 @@
 import requests
 
 
-url="http://localhost:6969/"
-# myobj = {'uuid': 'd5251247-50bc-4881-90a3-2c737308be17'}
-
-x = requests.get(url+"get-val/?uuid=d5251247-50bc-4881-90a3-2c737308be17",)
-
-print(x.text)
+url="http://greengardian-api.onrender.com/"
 
 
 data={
   "soil": 58,
   "temp": 28.4,
   "hum": 69.6,
-  "light": 862.0
+  "light": 1862.0
 }
 x = requests.post(url+"esp/d5251247-50bc-4881-90a3-2c737308be17", json = data)
 
 print(x.text)
+
+
+x = requests.get(url+"get-val/?uuid=d5251247-50bc-4881-90a3-2c737308be17",)
+
+print(x.text)
+
+# myobj = {'uuid': 'd5251247-50bc-4881-90a3-2c737308be17'}
+
